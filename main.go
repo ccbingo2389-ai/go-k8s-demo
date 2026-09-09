9aba88a08c9bac6d4c996e5adf47bcc6864f78f5
ccbingo2389-ai <ccbingo2389@gmail.com>
2026-09-09 17:42:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -229,7 +229,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	)
 
 	rootSpan.SetAttributes(attribute.Int("http.response.status_code", http.StatusOK))
-	fmt.Fprintln(w, "Hello from go-k8s-demo! Tracing + Logging + Metrics active.")
+	fmt.Fprintln(w, "Hello from go-k8s-demo! Tracing + Logging + Metrics active.202609091742")
 }
 
 func emitLog(ctx context.Context, sev apilog.Severity, body string, attrs ...attribute.KeyValue) {
